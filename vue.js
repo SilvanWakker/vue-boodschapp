73fa37ae739844e09c6191ce5796d8e28bd12c27
Silvan Wakker <wakkersilvan@gmail.com>
2019-09-09 12:27:38 +0200

    master - Add add item functionality

--- vue.js
+++ vue.js
@@ -3,21 +3,39 @@ Vue.component('shopping-list-item', {
   template: '<li>{{ item.text }}</li>'
 });
 
+
 var app = new Vue({
   el: '#app',
   data: {
+    newItem: null,
     shoppingListItems: [
-      { id: 1, text: 'Soup' },
-      { id: 2, text: 'Bread' },
-      { id: 3, text: 'Salami' },
+      {id: 1, text: 'Soup', checkedOff: false},
+      {id: 2, text: 'Bread', checkedOff: false},
+      {id: 3, text: 'Salami', checkedOff: false},
     ]
   },
   methods: {
-    clearList: function() {
+    /**
+     * Add item to shopping list items and clear the newItem input.
+     *
+     * @param event
+     */
+    addItem: function(event) {
+      this.shoppingListItems.push({
+        id: this.shoppingListItems.length + 1,
+        text: this.newItem,
+        checkedOff: false,
+      });
+      this.newItem = null;
+    },
+
+    /**
+     * Clears all shopping list items.
+     */
+    clearList: function () {
       if (confirm('Are you sure you want to delete all items in your shopping list?')) {
         this.shoppingListItems = [];
       }
     }
   }
 });
-
